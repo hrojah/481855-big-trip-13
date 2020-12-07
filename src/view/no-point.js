a8f5fce7b45fe4378aee0261c02800efff19eb9b
Vladimir Plotnikov <hrojah@gmail.com>
2020-12-07 21:52:01 +0300

    Проводит рефакторинг

--- src/view/no-point.js
+++ src/view/no-point.js
@@ -1,5 +1,6 @@
 import {createNoPointTemplate} from "./no-point-template";
 import Abstract from "./abstract";
+
 export default class NoPoint extends Abstract {
   getTemplate() {
     return createNoPointTemplate();
