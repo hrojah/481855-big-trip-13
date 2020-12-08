09df781b2376db6a0aea93a996ba75d434994ee7
Vladimir Plotnikov <hrojah@gmail.com>
2020-12-08 10:48:48 +0300

    Добавляет правки

--- src/view/trip-info-template.js
+++ src/view/trip-info-template.js
@@ -1,6 +1,6 @@
 export const createTripInfoTemplate = (events, routeList) => {
   const startDate = events[0].startTime.format(`MMM DD`);
-  const endDate = events[events.length - 1].startTime.format(`DD`);
+  const endDate = events[events.length - 1].startTime.format(`MMM DD`);
   return `<section class="trip-main__trip-info  trip-info">
            <div class="trip-info__main">
              <h1 class="trip-info__title">${routeList}</h1>
